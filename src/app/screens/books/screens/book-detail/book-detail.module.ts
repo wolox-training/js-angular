4eb42a2faaf65ef9e9b0796fb82bdc5f8a1f237b
JuanJo <jhanjhuse@gmail.com>
2021-10-12 18:19:43 -0500

    transations views

--- src/app/screens/books/screens/book-detail/book-detail.module.ts
+++ src/app/screens/books/screens/book-detail/book-detail.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { BookDetailRoutingModule } from './book-detail-routing.module';
 
+/* external modules */
+import { I18nModule } from '@i18n/i18n.module';
+
 @NgModule({
   declarations: [
     ...BookDetailRoutingModule.components
   ],
   imports: [
     CommonModule,
-    BookDetailRoutingModule
+    BookDetailRoutingModule,
+    /* external modules */
+    I18nModule,
+    I18nModule.forChild()
   ]
 })
 export class BookDetailModule { }
